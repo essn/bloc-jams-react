166603d766ab5cdb29f8d7ef1d0758a421da29f7
JessePledger <JessePledger@gmail.com>
2017-11-21 10:51:08 -0500

    Add Landing, App, and Nav styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
@@ -9,7 +10,10 @@ class App extends Component {
     return (
       <div className="App">
         <header>
-          <h1>Bloc Jams</h1>
+          <nav>
+            <Link to='/'><img src="./../assets/images/bloc_jams_logo.png" alt="Bloc Jams Landing" /></Link>
+            <Link to='/library'>Library</Link>
+          </nav>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
